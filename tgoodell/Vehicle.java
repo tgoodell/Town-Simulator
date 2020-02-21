4b54de50a82cdadc08e13195517ce73c7167bd1d
tristan <me@tgoodell.com>
2020-02-21 08:59:34 -0600

    1

---
+++ tgoodell/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle{
+    Simulator simulator; // This is your hook into the Simulator API
+    
+    public Vehicle(Simulator s){
+        this.simulator = s;
+    }
+
+    public void travel(){
+        
+    }
+}
